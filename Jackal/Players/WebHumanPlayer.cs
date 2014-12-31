a71b352f8e92ae4b0979655bbf9e8dd2ef538e04
Михаил <mifonrule@mail.ru>
2014-12-31 19:51:46 +0300

    Опять недочекин! исправлено!

---
+++ Jackal/Players/WebHumanPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jackal.Players
+{
+    public class WebHumanPlayer : IPlayer
+    {
+        private int _move;
+
+        public void OnNewGame()
+        {
+            _move = 0;
+        }
+
+        public void SetHumanMove(int moveNum)
+        {
+            _move = moveNum;
+        }
+
+        public int OnMove(GameState gameState)
+        {
+            return _move;
+        }
+    }
+}
